3ae3fea66daf55d282d871a4bd14f90478a43319
samehaisaa <a.semeh555@gmail.com>
2024-11-30 22:57:59 +0000

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -5,16 +5,13 @@ from langchain_core.messages import SystemMessage
 from langchain.chains.conversation.memory import ConversationBufferWindowMemory
 from langchain_groq import ChatGroq
 
-# Set environment variables
 from dotenv import load_dotenv
 import os
 
-# Load environment variables from .env file
 load_dotenv()
 
-# Access the API key
 os.environ["GROQ_API_KEY"] = os.getenv("GROQ_API_KEY")
-os.environ["TOKENIZERS_PARALLELISM"] = "false"  # To suppress HuggingFace warnings
+os.environ["TOKENIZERS_PARALLELISM"] = "false"  
 
 class GroqChatbot:
     def __init__(self, model="llama3-8b-8192", memory_length=5):
@@ -26,7 +23,6 @@ class GroqChatbot:
         self.memory = ConversationBufferWindowMemory(k=memory_length, memory_key="chat_history", return_messages=True)
         self.system_prompt = "You are a slay queen 2000s fashin icon conversational chatbot."
 
-        # Construct the chat prompt template
         self.prompt = ChatPromptTemplate.from_messages([
             SystemMessage(content=self.system_prompt),
             MessagesPlaceholder(variable_name="chat_history"),
